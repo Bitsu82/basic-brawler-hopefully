8d712000961f0070c0eef7e6780b02fd718d13a5
Bitsu82 <Cameron.Hardman@stu.burgmann.act.edu.au>
2026-05-13 22:06:09 +1000

    look man i got non movable characters now i think
    should i even try atp

--- main_loop.py
+++ main_loop.py
@@ -3,25 +3,31 @@ import sys
 pygame.init()
 
 #making the screen
-HIEGHT = 700
-WIDTH = 800
+HIEGHT = 800
+WIDTH = 1200
+screen = pygame.display.set_mode((WIDTH, HIEGHT))
 
 #setting up player character 
+guy1_og = pygame.image.load('guy.jpg').convert() #load sprite so we can transform it
+guy1_scaled = pygame.transform.scale_by(guy1_og, 0.25)
 guy1 = pygame.sprite.Sprite()
-guy2 = pygame.sprite.Sprite()
-
-guy1.image = pygame.image.load('')
+guy1.image = guy1_scaled
 guy1.rect = guy1.image.get_rect()
-guy2.image = pygame.image.load('')
+
+guy2_og = pygame.image.load('guy2.jpeg').convert() #load sprite so we can transform it
+guy2_scaled = pygame.transform.scale_by(guy2_og, 0.25)
+guy2 = pygame.sprite.Sprite()
+guy2.image = guy2_scaled
 guy2.rect = guy2.image.get_rect()
 
+screen.blit(guy1.image, (100, 200))
 
+guy1_group = pygame.sprite.GroupSingle(guy1)
+guy2_group = pygame.sprite.GroupSingle(guy2)
 
 
 while True:
-    pygame.display.set_mode((WIDTH, HIEGHT))
-
-
+ 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
             print("skibidi for now")
@@ -31,14 +37,13 @@ while True:
             print("skibidi for now")
         if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
             print("skibidi for now")
-
-
-
         if event.type == pygame.QUIT:
             pygame.quit
             sys.exit()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            pygame.quit
+            pygame.quit()
             sys.exit()
-
+    guy1_group.draw(screen)
+    pygame.display.update()
+    screen.fill((30, 30, 30))
             
